ef3bbfa24a4be7d0b70233e74a881df21a0b641e
KaidenTheCEO <kaidenmatthew@outlook.com>
2023-05-21 09:56:42 -0600

    Added config options to enable/disable every log

--- config/config.lua
+++ config/config.lua
@@ -1,4 +1,12 @@
 Config = {
-webhookUrl = "https://discord.com/api/webhooks/1109548804908257380/g4OhRW3n-gZCtzXR7ayDhiqvK3xUJM9cNT1B3WiM3fQzDH-Sjv212arh7IGVssJUCyfT"
+webhookUrl = "https://discord.com/api/webhooks/1109548804908257380/g4OhRW3n-gZCtzXR7ayDhiqvK3xUJM9cNT1B3WiM3fQzDH-Sjv212arh7IGVssJUCyfT",
+enableDeaths = false,
+enableChat = true,
+enableCommands = true,
+enableJoin = true,
+enableLeave = true,
+enableVMenuBans = true,
+enableVMenuKicks = true,
+enableVMenuNoClip = true
 }
 return config
--- server/server.lua
+++ server/server.lua
@@ -8,35 +8,79 @@ local function sendWebhookMessage(content, username)
 end
 
 -- Log player deaths
-AddEventHandler("playerDied", function()
-    local player = source
-    local playerName = GetPlayerName(player)
-    local deathMessage = "A player died."
-    sendWebhookMessage(deathMessage, playerName)
-end)
+if Config.enableDeaths then
+    AddEventHandler("playerDied", function()
+        local player = source
+        local playerName = GetPlayerName(player)
+        local deathMessage = "A player died."
+        sendWebhookMessage(deathMessage, playerName)
+    end)
+end
 
 -- Log chat messages
-AddEventHandler("chatMessage", function(source, name, message)
-    local chatMessage = string.format("%s", message)
-    sendWebhookMessage(chatMessage, name)
-end)
+if Config.enableChat then
+    AddEventHandler("chatMessage", function(source, name, message)
+        local chatMessage = string.format("%s", message)
+        sendWebhookMessage(chatMessage, name)
+    end)
+end
 
 -- Log commands used
-AddEventHandler("rconCommand", function(commandName, args)
-    local playerName = GetPlayerName(source)
-    local commandMessage = string.format("Command used: %s", commandName)
-    sendWebhookMessage(commandMessage, playerName)
-end)
+if Config.enableCommands then
+    AddEventHandler("rconCommand", function(commandName, args)
+        local playerName = GetPlayerName(source)
+        local commandMessage = string.format("Command used: %s", commandName)
+        sendWebhookMessage(commandMessage, playerName)
+    end)
+end
 
 -- Player joining server
-AddEventHandler("playerConnecting", function(playerName, setKickReason, deferrals)
-    local joinMessage = "**📥 Player joined the server: **" .. playerName
-    sendWebhookMessage(joinMessage, "Server")
-end)
+if Config.enableJoin then
+    AddEventHandler("playerConnecting", function(playerName, setKickReason, deferrals)
+        local joinMessage = "**📥 Player joined the server: **" .. playerName
+        sendWebhookMessage(joinMessage, "Server")
+    end)
+end
 
 -- Player leaving server
-AddEventHandler("playerDropped", function(reason)
-    local playerName = GetPlayerName(source)
-    local leaveMessage = "**📤  Player left the server: **" .. playerName
-    sendWebhookMessage(leaveMessage, "Server")
-end)
+if Config.enableLeave then
+    AddEventHandler("playerDropped", function(reason)
+        local playerName = GetPlayerName(source)
+        local leaveMessage = "**📤  Player left the server: **" .. playerName
+        sendWebhookMessage(leaveMessage, "Server")
+    end)
+end
+
+
+
+-- Function to log staff actions to the webhook
+local function logStaffAction(action, target, staffMember)
+    local message = string.format("**Staff Action**: %s\n**Target**: %s\n**Staff Member**: %s", action, target, staffMember)
+    sendWebhookMessage(message, "Server")
+end
+
+-- Log vMenu bans (Not Working?)
+if Config.enableVMenuBans then
+    RegisterServerEvent("vMenu:BanPlayer")
+    AddEventHandler("vMenu:BanPlayer", function(target, staffMember)
+        logStaffAction("Ban", target, staffMember)
+    end)
+end
+
+-- Log vMenu kicks (Not Working?)
+if Config.enableVMenuKicks then
+    RegisterServerEvent("vMenu:KickPlayer")
+    AddEventHandler("vMenu:KickPlayer", function(target, staffMember)
+        logStaffAction("Kick", target, staffMember)
+    end)
+end
+
+-- Log vMenu noclip (Not Working?)
+if Config.enableVMenuNoClip then
+    RegisterServerEvent("vMenu:ToggleNoClip")
+    AddEventHandler("vMenu:ToggleNoClip", function()
+        local playerName = GetPlayerName(source)
+        local message = string.format("**Noclip Activated**: %s", playerName)
+        sendWebhookMessage(message, "Server")
+    end)
+end
